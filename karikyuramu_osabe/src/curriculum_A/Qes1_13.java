2881d6aaf98ae5b286d9d3a5e8d7dc756391413e
game327 <game327@S-joBi18-048>
2025-03-05 21:40:23 +0900

    Qes_1

---
+++ karikyuramu_osabe/src/curriculum_A/Qes1_13.java
@@ -0,0 +1,79 @@
+package curriculum_A;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//下記9個をローカル変数として宣言のみしてください
+		
+		//バイト型
+		byte b = 1;
+		
+		System.out.println("------------------");
+		
+		System.out.println("変数bは"+ b +"です");
+		 
+		//短整数型
+		short s= 1;
+		
+		System.out.println("------------------");
+		
+		System.out.println("変数sは"+ s +"です");
+		 
+		//整数型
+		int i = 1;
+		
+		System.out.println("------------------");
+		
+		System.out.print("変数iは"+ i +"です");//+消すとエラーが出る…なぜ？
+        
+		//長整数型
+		long l= 1;
+		
+		System.out.println("------------------");
+		
+		System.out.println("変数lは"+ l +"です");
+        
+		//単精度浮動小数点数型
+		float f = 1e-1f;//なぜこの式になる？
+		
+		System.out.println("------------------");
+		
+		System.out.println("変数fは"+ f +"です");
+        
+		//倍精度浮動小数点数型
+		double d= 1.0;//分かっていない
+		
+		System.out.println("------------------");
+		
+		System.out.println("変数dは"+ d +"です");
+        
+		//文字型
+		char c= 65;//10進数
+		
+		//char c1 = 0b0100_0001; // 2進数(0b始まり、"_"で区切ることもできる)
+		//char c2 = 0101; // 8進数(0始まり)
+		//char c3 = 0x41; // 16進数(0x始まり)
+		
+		System.out.println("------------------");
+		
+		System.out.println("変数cは"+ c +"です");
+        
+		//文字列型
+		String str = "Hello";
+		
+		System.out.println("------------------");
+		
+		System.out.println("変数strは"+ str +"です");
+        
+		//ブーリアン型
+		boolean bl= true;
+		
+		System.out.println("------------------");
+		
+		System.out.println("変数blは"+ bl +"です");//ブーリアン型
+
+
+	}
+
+}
