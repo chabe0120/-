be067118200453ca0613dbdb69e44b90d34a48c9
game327 <game327@S-joBi18-048>
2025-03-06 21:19:09 +0900

    Qes22

--- karikyuramu_osabe/src/curriculum_A/Qes1_13.java
+++ karikyuramu_osabe/src/curriculum_A/Qes1_13.java
@@ -7,49 +7,63 @@ public class Qes1_13 {
 		//下記9個をローカル変数として宣言のみしてください
 		
 		//バイト型
-		byte b = 1;
+		byte b; 
+		b = 0;//初期値になる
+		b = 10;//下に行くほど更新する
 		
 		System.out.println("------------------");
 		
 		System.out.println("変数bは"+ b +"です");
 		 
 		//短整数型
-		short s= 1;
+		short s;
+		s= 0;
+		s= 100;
 		
 		System.out.println("------------------");
 		
 		System.out.println("変数sは"+ s +"です");
 		 
 		//整数型
-		int i = 1;
+		int i;
+		i= 0;
+		i= 1000;
 		
-		System.out.println("------------------");
+		System.out.println("------------------");//lnは改行コード
 		
-		System.out.print("変数iは"+ i +"です");//+消すとエラーが出る…なぜ？
+		System.out.println("変数iは"+ i +"です");//+消すとエラーが出る…なぜ？
         
 		//長整数型
-		long l= 1;
+		long l;
+		l= 0;
+		l= 10000;
 		
 		System.out.println("------------------");
 		
 		System.out.println("変数lは"+ l +"です");
-        
+		
 		//単精度浮動小数点数型
-		float f = 1e-1f;//なぜこの式になる？
+		float f;
+		f= 0.0f;
+		f= 9.5f;
 		
 		System.out.println("------------------");
 		
 		System.out.println("変数fは"+ f +"です");
         
 		//倍精度浮動小数点数型
-		double d= 1.0;//分かっていない
+		double d;
+		d= 0.0d;
+		d= 10.5d;
 		
 		System.out.println("------------------");
 		
 		System.out.println("変数dは"+ d +"です");
-        
+		
 		//文字型
-		char c= 65;//10進数
+		char c;
+		c= '\u0000';//10進数
+		c=  'a';
 		
 		//char c1 = 0b0100_0001; // 2進数(0b始まり、"_"で区切ることもできる)
 		//char c2 = 0101; // 8進数(0始まり)
@@ -60,19 +74,70 @@ public class Qes1_13 {
 		System.out.println("変数cは"+ c +"です");
         
 		//文字列型
-		String str = "Hello";
+		String str;
+		str = null;
+		str = "ハロー";
 		
 		System.out.println("------------------");
 		
 		System.out.println("変数strは"+ str +"です");
         
 		//ブーリアン型
-		boolean bl= true;
+		boolean bl;
+		bl= false;
+		bl= true;
 		
 		System.out.println("------------------");
 		
 		System.out.println("変数blは"+ bl +"です");//ブーリアン型
-
+		
+		long result;
+		result = 0;
+		result = b + s + i + (int)l;
+		System.out.println("------------------");
+		System.out.println("b + s + i + l は"+ (b + s + i + l) +"です");
+		
+		double result1;
+		result1 = 0;
+		result1 = d + f;
+		System.out.println("------------------");
+		System.out.println("d + f は"+ (Math.round(d + f)) +"です");
+		
+		String result2;
+		result2 = null;
+		result2 = c + str + bl;
+		System.out.println("------------------");
+		System.out.println("c + str + bl は"+ (c + str + bl) +"です");
+		
+		double result3;
+		result3 = 0;
+		result3 = b + s + i + l + f + d;
+		System.out.println("------------------");
+		System.out.println("b + s + i + l + f + d は"+ (Math.round(b + s + i + l + f + d)) +"です");
+		
+		double result4;
+		result4 = 0;
+		result4 = b * s * i * l;
+		System.out.println("------------------");
+		System.out.println("b * s * i * l は"+ (b * s * i * l) +"です");
+		
+		double result5;
+		result5 = 0;
+		result5 = d / s;
+		System.out.println("------------------");
+		System.out.println("d / s は"+ (d / s) +"です");
+		
+		double result6;
+		result6 = 0;
+		result6 = b - s;
+		System.out.println("------------------");
+		System.out.println("b - s は"+ (b - s) +"です");
+		
+		String num="20";
+		int num1=23;
+		
+		System.out.println("------------------");
+		System.out.println("ハローJAVA"+ (num + (int)num1) +"です");
 
 	}
 
