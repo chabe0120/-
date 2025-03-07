4a1ee46c9dabd4c711186e0b20c41bc3ff0dc42c
game327 <game327@S-joBi18-048>
2025-03-07 23:31:16 +0900

    Questclear001

--- karikyuramu_osabe/src/curriculum_A/Qes1_13.java
+++ karikyuramu_osabe/src/curriculum_A/Qes1_13.java
@@ -359,8 +359,6 @@ public class Qes1_13 {
 		System.out.println("------------------");
 	    System.out.println("身長は : " + Intfl + "cmです");
 	    
-	    
-	    
 	    //⑬12で変換した【年齢・身長】で【年齢が25もしくは身長が160以上】
 	    //であればtrueを出力してください
 	    
