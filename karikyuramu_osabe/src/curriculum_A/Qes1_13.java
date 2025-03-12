c10762cb8ce192325bfee894639d64f815075b86
game327 <game327@S-joBi18-048>
2025-03-12 22:31:01 +0900

    Qes_saiteisyutu

--- karikyuramu_osabe/src/curriculum_A/Qes1_13.java
+++ karikyuramu_osabe/src/curriculum_A/Qes1_13.java
@@ -115,7 +115,7 @@ public class Qes1_13 {
 		//④下記の通りにコンソール出力されるようにしてください
 		//上記で作成した変数を必ず使用すること
 
-		long result;
+		double result;
 		result = 0;
 		result = b + s + i + (int)l;
 		System.out.println("------------------");
@@ -171,45 +171,45 @@ public class Qes1_13 {
 		//⑥『』で囲われた人の情報を変数にして、formatの通りコンソールに出力してください
 		//ローカル変数に代入し○○に入れてください
 
-		String Str;
-		Str = null;
-		Str = "山田太郎";
+		String Name;
+		Name = null;
+		Name = "山田太郎";
 		
-		byte by;
-		by = 0;
-		by = 18;
+		int age;
+		age = 0;
+		age = 18;
 		
-		float fl;
-		fl = 0.0f;
-		fl = 170.5f;
+		float height;
+		height = 0.0f;
+		height = 170.5f;
 		
-		double fl1;
-		fl1 = 0.0d;
-		fl1 = 62.2d;
+		double weight;
+		weight = 0.0d;
+		weight = 62.2d;
 		
-		String STR;
-		STR = null;
-		STR = "寿司";
+		String EAT;
+		EAT = null;
+		EAT = "寿司";
 		
 		//「初めまして○○です」
 		System.out.println("------------------");
-		System.out.println("初めまして"+ Str +"です");
+		System.out.println("初めまして"+ Name +"です");
 		
 		//「年齢は○○歳です」
 		System.out.println("------------------");
-		System.out.println("年齢は"+ by +"歳です");
+		System.out.println("年齢は"+ age +"歳です");
 		
 		//「身長は○○cmです」
 		System.out.println("------------------");
-		System.out.println("身長は"+ fl +"cmです");
+		System.out.println("身長は"+ height +"cmです");
 		
 		//「体重は○○kgです」
 		System.out.println("------------------");
-		System.out.println("体重は"+ fl1 +"kgです");
+		System.out.println("体重は"+ weight +"kgです");
 		
 		//「好きな食べ物は○○です」
 		System.out.println("------------------");
-		System.out.println("好きな食べ物は"+ STR +"です");
+		System.out.println("好きな食べ物は"+ EAT +"です");
 
 		
 		//⑦6で作成した自己紹介に続いてBMIが出力されるようにしてください
@@ -217,128 +217,130 @@ public class Qes1_13 {
 		//ただし計算は数値を直書きせず、全て変数を使ってすること
 
 		double result25;
-		fl = 1.705f;
-		result25 = Math.pow(fl, fl);
+		height = 1.705f;
+		result25 = Math.pow(height, height);
 		System.out.println("------------------");
-		System.out.println("BMIは"+ String.format("%.1f",fl1 / (fl * fl)) +"です");
+		System.out.println("BMIは"+ String.format("%.1f",weight / (height * height)) +"です");
 		
 		//⑧6で宣言した変数に再代入し下記の通りコンソールに出力してください
 
 		//初めまして鈴木一郎です
-		Str = "鈴木一郎";
+		Name = "鈴木一郎";
 		
 		//年齢は24歳です
-		by = 24;
+		age = 24;
 		
 		//身長168.5cmです
-		fl = 168.5f;
+		height = 168.5f;
 		
 		//体重は64.2kgです
-		fl1 = 64.2d;
+		weight = 64.2d;
 		
 		//好きな食べ物はオムライスです
-		STR = "オムライス";
+		EAT = "オムライス";
 		
 		//BMIは22.6です
-		fl = 168.5f;
-		fl1 = 64.2d;
+		height = 168.5f;
+		weight = 64.2d;
 		
 		System.out.println("------------------");
-		System.out.println("初めまして"+ Str +"です");
+		System.out.println("初めまして"+ Name +"です");
 		
 
 		
 		System.out.println("------------------");
-		System.out.println("年齢は"+ by +"歳です");
+		System.out.println("年齢は"+ age +"歳です");
 		
 		
 		System.out.println("------------------");
-		System.out.println("身長は"+ fl +"cmです");
+		System.out.println("身長は"+ height +"cmです");
 		
 				
 		System.out.println("------------------");
-		System.out.println("体重は"+ fl1 +"kgです");
+		System.out.println("体重は"+ weight +"kgです");
 				
 		System.out.println("------------------");
-		System.out.println("好きな食べ物は"+ STR +"です");
+		System.out.println("好きな食べ物は"+ EAT +"です");
 		
 		//身長をｍに直した。
 		double result22;
-		fl = 1.685f;
+		height = 1.685f;
 		
-		result22 = Math.pow(fl, fl);
+		result22 = Math.pow(height, height);
 		System.out.println("------------------");
-		System.out.println("BMIは"+ String.format("%.1f",fl1 / (fl * fl)) +"です");
+		System.out.println("BMIは"+ String.format("%.1f",weight / (height * height)) +"です");
 		
 		//⑨8で使用した変数【年齢・身長・体重】の数値を和算で自己代入し、
 		//下記の通りコンソールに出力してください
-		Str = "鈴木一郎";
+		Name = "鈴木一郎";
 		
 		//年齢は48歳です
-		by = 24;
+		age = (age + 24);
 		
 		//身長337.0cmです
-		fl = 168.5f;
+		height = 168.5f;
+		height = (float) (height + 168.5f);
 		
 		//体重は64.2kgです
-		fl1 = 64.2d;
+		weight = 64.2d;
+		weight = weight + 64.2f;
 		
 		//好きな食べ物はオムライスです
-		STR = "オムライス";
+		EAT = "オムライス";
 		
 		System.out.println("------------------");
-		System.out.println("初めまして"+ Str +"です");
+		System.out.println("初めまして"+ Name +"です");
 		
 		System.out.println("------------------");
-		System.out.println("年齢は"+ (by + by) +"歳です");
+		System.out.println("年齢は"+ (age) +"歳です");
 		
 		System.out.println("------------------");
-		System.out.println("身長は"+ String.format("%.1f", fl + fl) +"cmです");
+		System.out.println("身長は"+ String.format("%.1f", height) +"cmです");
 		
 		System.out.println("------------------");
-		System.out.println("体重は"+ String.format("%.1f",fl1 + fl1) +"kgです");
+		System.out.println("体重は"+ String.format("%.1f",weight) +"kgです");
 		
 		System.out.println("------------------");
-		System.out.println("好きな食べ物は"+ STR +"です");
+		System.out.println("好きな食べ物は"+ EAT +"です");
 		
 		//身長をｍに直した。
 		double result29;
-		fl = 3.37f;
+		height = 3.37f;
 		
 		//BMIは11.31です
-		result29 = Math.pow(fl , fl);
+		result29 = Math.pow(height , height);
 		System.out.println("------------------");
-		System.out.println("BMIは"+ String.format("%.2f",(fl1 + fl1) / (fl * fl)) +"です");
+		System.out.println("BMIは"+ String.format("%.2f",(weight) / (height * height)) +"です");
 		
 		//➉8で使用した年齢が25歳以上ならtrueが出力されるようにしてください。ただしif文は使いません
-
-		boolean result55= (by >= 25);
+		age = 24;
+		
+		boolean result55= (age >= 25);
 		System.out.println("------------------");
-		result55 = true;
 		System.out.println("年齢が25歳以上なら"+ result55 +"です");//ブーリアン型
 		
 		//⑪8で使用した【年齢・身長・体重】を文字列型に型変換し繋げて出力してください
 		
 		//年齢
 		//byte→String
-		by = 18;
+		age = 18;
 		
-		String strby = String.valueOf(by);
+		String strby = String.valueOf(age);
 		System.out.println("------------------");
 	    System.out.println("年齢は : " + strby + "歳です");
 		
 		//身長
 		//float→String
-		fl = 170.5f;
-		String strfl = String.valueOf(fl);
+	    height = 170.5f;
+		String strfl = String.valueOf(height);
 		System.out.println("------------------");
 	    System.out.println("身長は : " + strfl + "cmです");
 		
 		//体重
 		//double→String
-		fl1 = 62.2d;
+	    weight = 62.2d;
 		
-		String strfl1 = String.valueOf(fl1);
+		String strfl1 = String.valueOf(weight);
 		System.out.println("------------------");
 	    System.out.println("体重は : " + strfl1 + "kgです");
 	    
@@ -347,14 +349,14 @@ public class Qes1_13 {
 		//年齢
 		//byte→String
 		
-		Integer number = Integer.valueOf(by);
+		Integer number = Integer.valueOf(age);
 		System.out.println("------------------");
 	    System.out.println("年齢は : " + number + "歳です");
 		
 		//身長
 		//float→String
 	    
-	    Integer aaa = Math.round(fl);
+	    Integer aaa = Math.round(height);
 		Integer Intfl = Integer.valueOf(aaa);
 		System.out.println("------------------");
 	    System.out.println("身長は : " + Intfl + "cmです");
