a255b181b1a0bcf5c50073305bb92fe157eeedba
chabe0120 <chabe3141@gmail.com>
2025-06-20 23:17:47 +0900

    2025/06/20 再提出

--- karikyuramu_osabe/src/karikyuramu_osabe/AnimalName.java
+++ karikyuramu_osabe/src/karikyuramu_osabe/AnimalName.java
@@ -2,45 +2,48 @@ package karikyuramu_osabe;
 
 public class AnimalName {
 	
-	public void MainAnimal(String[] Animalarray) {
-		for(int i = 0; i < Animalarray.length; i++) {
-			String[] Animals = Animalarray[i].split(":");
+	public void MainAnimal(String[] animalarray) {
+		for(int i = 0; i < animalarray.length; i++) {
+			String[] Animals = animalarray[i].split(":");
 			
 			System.out.println("動物名："+ Animals[0]);
 			System.out.println("体長："+ Animals[1] +"m");
 			System.out.println("速度："+ Animals[2] +"km/h");
-			System.out.println("学名："+ this.getName(Animals[0]) +"\n");
+			System.out.println("学名："+ this.getScientificName(Animals[0]) +"\n");
 		}
 	}
 	
-	public  String getName(String Name){
+	public  String getScientificName(String animalName){
+		// 動物を格納する変数を初期化
+		String scientificName = null;
+		
+		// animalName : コンソールで入力された値
+		switch (animalName) {
+		case "ライオン":
+			scientificName = "パンテラ レオ";
+			break;
 	
-	switch (Name) {
-	case "ライオン":
-		Name = "パンテラ レオ";
-		break;
+		case "ゾウ":
+			scientificName ="ロキソドンタ・サイクロティス";
+			break;
 	
-	case "ゾウ":
-		Name ="ロキソドンタ・サイクロティス";
-		break;
-
-	case "パンダ":
-		Name ="アイルロポダ・メラノレウカ";
-		break;
-
-	case "チンパンジー":
-		Name ="パン・トゥログロディテス";
-		break;
-
-	case "シマウマ":
-		Name ="チャップマンシマウマ";
-		break;
-
-	case "インコ":
-		Name ="不明";
-		break;
+		case "パンダ":
+			scientificName ="アイルロポダ・メラノレウカ";
+			break;
+	
+		case "チンパンジー":
+			scientificName ="パン・トゥログロディテス";
+			break;
+	
+		case "シマウマ":
+			scientificName ="チャップマンシマウマ";
+			break;
+	
+		case "インコ":
+			scientificName ="不明";
+			break;
 	}
-	return Name;
-
+		// 格納した動物情報を返却
+		return scientificName;
 	}
 }
